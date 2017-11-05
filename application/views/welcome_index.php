732433f560238e81942781c9d6cd83b5498569ad
Hansell Ramos <hansell.x.ramos.-nd@disney.com>
2017-11-05 10:55:49 -0500

    Added create account home menu

--- application/views/welcome_index.php
+++ application/views/welcome_index.php
@@ -11,6 +11,9 @@
         <div id="container">
             <h1>Bienvenido <?php echo "$account->name $account->lastname ($account->username)"; ?></h1>
             <div id="body">
+                <?php if($account->is_admin){ ?>
+                <?php echo anchor('account/add','Create Account'); ?>
+                <?php } ?>
                 <?php echo anchor('product/index','Lista de Productos',['class'=>'']); ?>
             </div>
 
